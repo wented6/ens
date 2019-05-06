52535c185ddfb2dc64c0cd271fb3de057cc312ba
wented6 <44713416+wented6@users.noreply.github.com>
2019-05-06 23:28:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,50 @@
-﻿
+const D1 = require('discord.js');
+const D2 = require('discord.js');
+
+
+
+
+
+const p1 = new D1.Client();
+const p2 = new D2.Client();
+
+
+
+
+
+
+    console.log("oNNN1");
+p1.on('guildMemberAdd', member => {
+const fuli= member.guild.channels.get("573399652305731596");
+if(!fuli) return;
+if(fuli) {
+setTimeout(() => fuli.send(`**Welcome to Spoil.**`), 4000)
+}
+});
+
+   console.log("oNNN2");
+p2.on('guildMemberAdd', member => {
+const fuli= member.guild.channels.get("573399652305731596");
+if(!fuli) return;
+if(fuli) {
+setTimeout(() => fuli.send(`**Welcome to Spoil.**`), 3800)
+}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+p1.login(process.env.B);
+p2.login(process.env.B2);
+
