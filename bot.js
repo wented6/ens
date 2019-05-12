8ab20c5c6bfc611bcc76c2c837aaa8354d13cdf5
wented6 <44713416+wented6@users.noreply.github.com>
2019-05-13 02:30:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,19 +15,19 @@ const p2 = new D2.Client();
 
     console.log("oNNN1");
 p1.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("573399652305731596");
+const fuli= member.guild.channels.get("564191993018974209");
 if(!fuli) return;
 if(fuli) {
-setTimeout(() => fuli.send(`**Welcome to Spoil.**`), 4000)
+setTimeout(() => fuli.send(`**Welcome to Spoil.**`), 9200)
 }
 });
 
    console.log("oNNN2");
 p2.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("573399652305731596");
+const fuli= member.guild.channels.get("564191993018974209");
 if(!fuli) return;
 if(fuli) {
-setTimeout(() => fuli.send(`**Welcome to Spoil.**`), 5000)
+setTimeout(() => fuli.send(`**Welcome to Spoil.**`), 9000)
 }
 });
 
