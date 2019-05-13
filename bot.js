a59926703d0a5de9a7c4d23b170364fc970f4c53
wented6 <44713416+wented6@users.noreply.github.com>
2019-05-13 19:58:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const D1 = require('discord.js');
 const D2 = require('discord.js');
+const D3 = require('discord.js');
 
 
 
@@ -7,6 +8,7 @@ const D2 = require('discord.js');
 
 const p1 = new D1.Client();
 const p2 = new D2.Client();
+const p3 = new D3.Client();
 
 
 
@@ -15,7 +17,7 @@ const p2 = new D2.Client();
 
     console.log("oNNN1");
 p1.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("564191993018974209");
+const fuli= member.guild.channels.get("577536069692817418");
 if(!fuli) return;
 if(fuli) {
 setTimeout(() => fuli.send(`**Welcome to Bùbbles.**`), 9200)
@@ -24,7 +26,7 @@ setTimeout(() => fuli.send(`**Welcome to Bùbbles.**`), 9200)
 
    console.log("oNNN2");
 p2.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("564191993018974209");
+const fuli= member.guild.channels.get("577536069692817418");
 if(!fuli) return;
 if(fuli) {
 setTimeout(() => fuli.send(`**Welcome to Bùbbles.**`), 9000)
@@ -35,6 +37,14 @@ setTimeout(() => fuli.send(`**Welcome to Bùbbles.**`), 9000)
 
 
 
+   console.log("oNNN3");
+p3.on('guildMemberAdd', member => {
+const fuli= member.guild.channels.get("577536069692817418");
+if(!fuli) return;
+if(fuli) {
+setTimeout(() => fuli.send(`**Welcome to Bùbbles.**`), 9200)
+}
+});
 
 
 
@@ -47,4 +57,5 @@ setTimeout(() => fuli.send(`**Welcome to Bùbbles.**`), 9000)
 
 p1.login(process.env.B);
 p2.login(process.env.B2);
+p3.login(process.env.B3);
 
