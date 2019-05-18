27b2b593f65b0b672e8c58c07d4d27399b11135a
wented6 <44713416+wented6@users.noreply.github.com>
2019-05-19 00:10:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ const D2 = require('discord.js');
 const D3 = require('discord.js');
 const D4 = require('discord.js');
 const D5 = require('discord.js');
+const D6 = require('discord.js');
+const D7 = require('discord.js');
 
 
 
@@ -13,6 +15,8 @@ const p2 = new D2.Client();
 const p3 = new D3.Client();
 const p4 = new D4.Client();
 const p5 = new D5.Client();
+const p6 = new D6.Client();
+const p7 = new D7.Client();
 
 
 
@@ -75,6 +79,25 @@ setTimeout(() => fuli.send(`**Welcome to Bùbbles.**`), 9600)
 });
 
 
+   console.log("oNNN6");
+p6.on('guildMemberAdd', member => {
+const fuli= member.guild.channels.get("577536069692817418");
+if(!fuli) return;
+if(fuli) {
+setTimeout(() => fuli.send(`**Welcome to Bùbbles.**`), 9800)
+}
+});
+
+
+
+   console.log("oNNN7");
+p7.on('guildMemberAdd', member => {
+const fuli= member.guild.channels.get("577536069692817418");
+if(!fuli) return;
+if(fuli) {
+setTimeout(() => fuli.send(`**Welcome to Bùbbles.**`), 10000)
+}
+});
 
 
 
@@ -84,4 +107,5 @@ p2.login(process.env.B2);
 p3.login(process.env.B3);
 p4.login(process.env.B4);
 p5.login(process.env.B5);
-
+p6.login(process.env.B6);
+p7.login(process.env.B7);
